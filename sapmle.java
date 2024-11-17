740d90dce40736bb29b32cd31d6707df3a2d9c70
Darshan252508 <darshangowda252508@gmail.com>
2024-11-17 12:17:21 +0530

    modifiesd

--- sapmle.java
+++ sapmle.java
@@ -1,5 +1,6 @@
 public class sapmle {
     public static void main(String[] args) {
-        System.out.println("new");
+        System.out.println("new  ");
+        System.out.println("another new line");
     }
 }
